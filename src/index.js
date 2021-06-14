997803b56c31f7e1d4db21b8d98f9b8d1bd65c05
Monicacholico <monicachol@hotmail>
2021-06-14 15:38:09 -0500

    saving

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Import the React and ReacDOM libraries
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+// Create a react component
+
+
+// Take the react comopnent and show it on the screen
