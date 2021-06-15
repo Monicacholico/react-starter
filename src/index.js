2747584716ce6ef4d9e86f5e897807ea4a781119
Monicacholico <monicachol@hotmail>
2021-06-14 23:42:07 -0500

    props children

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import CommentDetail from './CommentDetail';
 import Component from './components';
 import Input from './input';
 import faker from 'faker'
+import ApprovalCard from './ApprovalCard';
 
 // Create a react component
 
@@ -16,12 +17,15 @@ if (module.hot) {
           <div>
          {/* <Input/> */}
         {/* <Component/> */}
+        <ApprovalCard>
         <CommentDetail
             image= {faker.image.avatar()}
             author={"Jessie"}
             date={new Date().toLocaleTimeString()}
             comment={"Excellent post!"}
         />
+        </ApprovalCard>
+        <ApprovalCard>
         <CommentDetail
             image= {faker.image.avatar()}
             author={"Sam"}
@@ -29,20 +33,25 @@ if (module.hot) {
             comment={"HG!"}
 
         />
+        </ApprovalCard>
+        <ApprovalCard>
         <CommentDetail
             image= {faker.image.avatar()}
             author={"Jaime"}
             date={new Date().toLocaleTimeString()}
             comment={"What a post!"}
         />
+        </ApprovalCard>
+        <ApprovalCard>
         <CommentDetail
             image= {faker.image.avatar()}
             author={"Monica"}
             date={new Date().toLocaleTimeString()}
             comment={"Hey!"}
         />
+        </ApprovalCard>
           </div>
-      )
+      );
   };
 
 // Take the react component and show it on
